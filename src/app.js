a0d763957dd48550855ddcc0afa83b8bad46180b
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-06-24 19:19:31 -0300

    Obrigatoriedades Completas

--- src/app.js
+++ src/app.js
@@ -6,15 +6,12 @@ const app = express();
 const PORT = 5000;
 app.use(cors());
 const log = console.log;
+app.use(express.json());
 
 
-/* Objetos: */
+/* Objetos e funções: */
 
 const UserList = [
-    {
-        username: 'bobesponja', 
-        avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png" 
-    },
     {
         username: 'Vinni_Evangelista', 
         avatar: "https://pbs.twimg.com/media/FDo4mQQVEAIcWh6.jpg" 
@@ -23,17 +20,25 @@ const UserList = [
 
 const TweetList = [
     {
-        username: "bobesponja",
-        tweet: "Eu amo hambúrguer de siri!"
-    },
-    {
         username: "Vinni_Evangelista",
         tweet: "Será se esse negócio vai prestar???"
     }
 ]
 
+function validação(val1, val2){
+    let String = "";
+    if (!val1 || !val2) {
+        return(true)
+    } else if (typeof(val1) !== typeof(String) ||  typeof(val2) !== typeof(String) ) {
+        return (true)
+    } else {
+        return(false);
+    }
+}
+
 
 /* Rotas */
+/*Gets */
 app.get('/tweets', (req,res) => {
     let Tweets = [];
     for (let i = 0; i < TweetList.length; i++) {
@@ -48,11 +53,42 @@ app.get('/tweets', (req,res) => {
     res.send(Tweets)
 })
 
-app.get('/:name', (req, res) => {
-	const name = req.params.name;
+app.get('/users', (req,res) => {
+    res.send(UserList);
+})
 
-    res.send('Hello ' + name); 
+
+/* Posts */
+app.post('/sign-up', (req, res) => {
+    let body = req.body;
+
+    if (validação(body.username,body.avatar)) {
+        return res.status(400).send('Todos os campos são obrigatórios!')
+    }
+
+    UserList.push(body)
+	res.status(201).send("OK")
 });
+
+app.post('/tweets', (req, res) => {
+    let body = req.body;
+
+    let users = [];
+    for (let i = 0; i < UserList.length; i++) {
+        users.push(UserList[i].username)
+    }
+
+    if (validação(body.username,body.tweet)) {
+        return res.status(400).send('Todos os campos são obrigatórios!')
+    }
+    if (!users.includes(body.username)) {
+        return res.status(401).send('UNAUTHORIZED');
+    }
+
+    TweetList.push(body);
+    res.status(201).send("OK")
+});
+
 
 /* Listen!! */
 app.listen(PORT, () => log(`Servidor rodando na porta ${PORT}`))
